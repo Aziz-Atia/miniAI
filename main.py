aa5414aebc155d97ae5d4f7b0da1cfcf611c5ecc
Atia Binti Aziz <you@example.com>
2025-09-22 05:11:13 +0600

    multithread conversation working: nremaining else if condition for verbose

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from google import genai
-
-
+import sys
 import os
 from dotenv import load_dotenv
+from google.genai import types
+
+
 def main():
     print("Hello from miniai!")
 
@@ -10,10 +12,13 @@ def main():
 if __name__ == "__main__":
     main()
 load_dotenv()
-
+inputContents = sys.argv[1]
 client = genai.Client(api_key= os.environ.get("GEMINI_API_KEY")) #except writing api_key=api_key, writing this way
+messages = [
+    types.Content(role="user", parts=[types.Part(text=inputContents)]),
+]
 response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+    model='gemini-2.0-flash-001', contents=messages
 )
 print(response.text)
 print("Prompt tokens:", response.usage_metadata.prompt_token_count)
